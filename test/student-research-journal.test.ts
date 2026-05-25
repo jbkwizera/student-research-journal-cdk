3b5c8f6a7c74588ff382e6db30267e9872fde8e2
Kwizera, Jean Baptiste <jeanbaptistekwi@gmail.com>
2026-05-24 18:24:52 -0700

    chore: initial commit

---
+++ test/student-research-journal.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StudentResearchJournal from '../lib/student-research-journal-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/student-research-journal-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StudentResearchJournal.StudentResearchJournalStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
